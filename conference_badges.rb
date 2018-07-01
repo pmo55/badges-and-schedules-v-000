688fc7d35c8c28849ff0851bec90ebdcb4fbcc14
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-01 04:22:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  batch=[]
+names.each do |name|
+  batch<< badge_maker(name)
+end
+batch
+end
+def assign_rooms(names)
+  rooms=[]
+  counter =1
+  names.collect do |name|
+ rooms <<  "Hello, #{name}! You'll be assigned to room #{counter}!"
+    counter +=1
+end
+rooms 
+end
+def printer(names)
+  
+batch_badge_creator(names)
+
+end
